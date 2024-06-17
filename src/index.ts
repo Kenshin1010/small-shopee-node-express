eb42e54ffb228bb3da3f9e879a27f781018a839d
anh.do <tuananh9h@gmail.com>
2024-06-17 15:59:29 +0700

    index.ts change

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ const app = express();
 
 console.log("App is starting...");
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.get("/", (req, res) => {
   console.log("Received request on /");
   res.send("Hello, World!");
