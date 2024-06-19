53851e99d841b3f296a3fb81637cb98a587fa291
anh.do <tuananh9h@gmail.com>
2024-06-19 09:52:41 +0700

    mongoose connect to MongoDB

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://root:1234@booksstoreapi.5kligic.mongodb.net/?retryWrites=true&w=majority&appName=BooksStoreAPI";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+import { mongoDBURL, PORT } from "./config";
 
 const app = express();
+app.use(express.json());
 
 console.log("App is starting...");
 
@@ -10,14 +13,20 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  console.log("Received request on /");
-  res.send("Hello, World!");
+  console.log(req);
+  return res.status(234).send("Store Books API");
 });
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Running at http://localhost:${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 module.exports = app;
