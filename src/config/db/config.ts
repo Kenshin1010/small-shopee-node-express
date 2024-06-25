5553f16fb45e260aec1d0e69f6831888abcd398f
anh.do <tuananh9h@gmail.com>
2024-06-25 14:41:00 +0700

    fix config mongodb

--- src/config/db/config.ts
+++ src/config/db/config.ts
@@ -2,6 +2,5 @@ import dotenv from "dotenv";
 
 // Load environment variables from .env file
 dotenv.config();
-export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5555;
-export const mongoDBURL =
-  process.env.MONGODB_URL || "mongodb://localhost:27017/default";
+export const PORT = process.env.PORT;
+export const mongoDBURL = process.env.MONGODB_URL!;
